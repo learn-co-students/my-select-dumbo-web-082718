bb643d5119eca248220f3642a45a94effe936ea9
Nasser Sanou <nassersanou23@gmail.com>
2018-08-23 01:54:01 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
+
 def my_select(collection)
- # your code here!
-end
+  arr = []
+  
+  if block_given? 
+  i = 0 
+
+  while i < collection.length 
+    yield collection[i]
+   
+  arr << collection
+  end 
+   i += 1 
+ 
+  else 
+
+  puts "Hey! No block was given!"
+  end 
+en
